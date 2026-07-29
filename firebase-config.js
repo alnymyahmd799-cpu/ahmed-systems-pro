f1ba7fa65badad0a92cb1ff37345d0423effeb7e
Ahmed M. Ali <ahmdmhmd150125390@stud.ntu.edu.iq>
2026-07-29 05:29:44 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD4QL1ianVF_HUSw-E6xTfkT-7-NOyCwW4",
+  authDomain: "ahmed-system.firebaseapp.com",
+  databaseURL: "https://ahmed-system-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ahmed-system",
+  storageBucket: "ahmed-system.firebasestorage.app",
+  messagingSenderId: "1023342978134",
+  appId: "1:1023342978134:web:b8523d11f59b4c581ac7f7"
+};
